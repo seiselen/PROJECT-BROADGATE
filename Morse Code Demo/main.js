d666ab15424674d855d6e23737f22ea8f604b5e3
Steven Eiselen <seiselen24@email.arizona.edu>
2021-09-05 21:37:25 -0700

    improved morse code gen

--- Morse Code Demo/main.js
+++ Morse Code Demo/main.js
@@ -1,19 +1,28 @@
-var morseGen;
-var UI_textIn, UI_enterText;
+//######################################################################
+//>>> Variable / Data-Structure Declarations
+//######################################################################
+var morseGen, UI_textIn, UI_enterText;
 
+//######################################################################
+//>>> Loader / Launcher / UI-UX Functions
+//######################################################################
 function setup(){
-  createCanvas(1000,500).parent("viz");
+  createCanvas(800,720).parent("viz");
   textFont('Courier New');
-
-  UI_textIn    = createInput('').size(500).parent("ui");
-  UI_enterText = createButton('Translate').mousePressed(UIHandle_enterText).parent("ui");
-
+  initUI();
   morseGen = new MorseCodeGenerator();
-}
+} // Ends P5JS Function setup
 
 function draw(){
   background(60);
   morseGen.render();
-}
+} // Ends P5JS Function draw
 
-function UIHandle_enterText(){morseGen.sentenceToConsole(UI_textIn.value());}
+function initUI(){
+  UI_textIn    = createInput('').size(500).parent("ui");
+  UI_enterText = createButton('Translate Message!').mousePressed(UIHandle_enterText).parent("ui");
+} // Ends Function initUI
+
+function UIHandle_enterText(){
+  morseGen.sentenceToConsole(UI_textIn.value());
+} // Ends Function UIHandle_enterText
--- Morse Code Demo/morseGen.js
+++ Morse Code Demo/morseGen.js
@@ -1,50 +1,99 @@
+//######################################################################
+//>>> MorseCodeGenerator Class Definition
+//######################################################################
 class MorseCodeGenerator{
   constructor(){
-    this.map = {
-      'a' : ".-",    'b' : "-...",   'c' : "-.-.",   'd' : "-..",    'e' : ".",    
-      'f' : "..-.",  'g' : "--.",    'h' : "....",   'i' : "..",     'j' : ".---",
-      'k' : "-.-",   'l' : ".-..",   'm' : "--",     'n' : "-.",     'o' : "---",
-      'p' : ".---.", 'q' : "--.-",   'r' : ".-.",    's' : "...",    't' : "-",    
-      'u' : "..-",   'v' : "...-",   'w' : ".--",    'x' : "-..-",   'y' : "-.--",
-      'z' : "--..",  '1' : ".----",  '2' : "..---",  '3' : "...--",  '4' : "....-",
-      '5' : ".....", '6' : "-....",  '7' : "--...",  '8' : "---..",  '9' : "----.",
-      '0' : "-----", ',' : "--..--", '.' : ".-.-.-", '?' : "..--..", ' ' : "  ",
-    };
-
-    this.prefxEngl = "English:    ";
-    this.prefxMors = "Morse Code: ";
-
-    this.sent_engl = ""; //"all your base are belong to us";
+    this.prefxEngl = "Input Message (English):";
+    this.prefxMors = "Translation (Morse Code):";
+    this.sent_engl = "Enter Your Message Here";
     this.sent_mors = "";
     this.sentenceToConsole(this.sent_engl);
-  }
+  } // Ends Constructor
 
   sentenceToConsole(s){
-    let sent = "";
+    s = s.toLowerCase();
+    this.sent_engl = "";
+    this.sent_mors = "";
+    let curChar, morChar;
     for (var i = 0; i < s.length; i++) {
-      sent += this.map[s.charAt(i)];
-      if(i<s.length-1){sent+="  ";}
+      curChar = s.charAt(i);
+      this.sent_engl += curChar;
+      this.sent_mors += this.charToMorse(curChar);
+      if(i<s.length-1){this.sent_mors+="  ";}
     }
-    console.log(sent);
-    this.sent_engl = s;
-    this.sent_mors = sent;
-  }
+  } // Ends Function sentenceToConsole
 
   render(){
     let rand = noise(frameCount*0.1);
+    let midH = height/2;
+    stroke(lerp(255,120,rand)); fill(lerp(120,255,rand));
+    line(0,midH,width,height/2);
+    textSize(32); text(this.prefxEngl,16,32); text(this.prefxMors,16,midH+32);
+    textSize(24); text(this.sent_engl,32,48,width-32,midH-32); text(this.sent_mors,32,midH+48,width-32,height-32);
+  } // Ends Function render
 
-    stroke(lerp(255,120,rand));
-    fill(lerp(120,255,rand));
-
-    let h2 = height/2;
-
-    line(0,h2,width,height/2);
-
-    textSize(32);text(this.prefxEngl,16,32);
-    textSize(24);text(this.sent_engl,32,48,width-32,h2-32);
-
-    textSize(32);text(this.prefxMors,16,h2+32);
-    textSize(24);text(this.sent_mors,32,h2+48,width-32,height-32);
-  }
-
+  /*--------------------------------------------------------------------
+  |>>> Function charToMorse : Implementation [Update] Note:                                            
+  +---------------------------------------------------------------------
+  | > Decided to replace the original implementation of a HashMap (via a
+  |   JS object) with a mapping function utilizing a Switch Statement
+  |   (perhaps the biggest one I ever wrote, but I digress); as to make
+  |   the mappings clearer to see and easier to install additional ones.
+  +-------------------------------------------------------------------*/
+  charToMorse(curChar){
+    switch(curChar){
+      /*### Alphabet ###*/
+      case 'a' : return ".-";
+      case 'b' : return "-...";
+      case 'c' : return "-.-.";
+      case 'd' : return "-..";
+      case 'e' : return ".";
+      case 'f' : return "..-.";
+      case 'g' : return "--.";
+      case 'h' : return "....";
+      case 'i' : return "..";
+      case 'j' : return ".---";
+      case 'k' : return "-.-";
+      case 'l' : return ".-..";
+      case 'm' : return "--";
+      case 'n' : return "-.";
+      case 'o' : return "---";
+      case 'p' : return ".---.";
+      case 'q' : return "--.-";
+      case 'r' : return ".-.";
+      case 's' : return "...";
+      case 't' : return "-";
+      case 'u' : return "..-";
+      case 'v' : return "...-";
+      case 'w' : return ".--";
+      case 'x' : return "-..-";
+      case 'y' : return "-.--";
+      case 'z' : return "--..";
+      /*### Numbers ###*/
+      case '1' : return ".----";
+      case '2' : return "..---";
+      case '3' : return "...--";
+      case '4' : return "....-";
+      case '5' : return ".....";
+      case '6' : return "-....";
+      case '7' : return "--...";
+      case '8' : return "---..";
+      case '9' : return "----.";
+      case '0' : return "-----";
+      /*### (Some) Punctuation ###*/
+      case ',' : return "--..--";
+      case '.' : return ".-.-.-";
+      case '?' : return "..--..";
+      case '!' : return "-.-.--";
+      case '\'': return ".----.";
+      case '/' : return "-..-.";
+      case ':' : return "---...";
+      case ';' : return "-.-.-.";
+      case '(' : return "-.--.";
+      case ')' : return "-.--.-";
+      case ' ' : return "  ";
+      /*### Non-Supported Chars (i.e. Error) ###*/
+      default  : return "........";
+    } // Ends [the biggest] Switch [that I ever wrote]
+  } // Ends Function charToMorse
 } // Ends Class MorseCodeGenerator
